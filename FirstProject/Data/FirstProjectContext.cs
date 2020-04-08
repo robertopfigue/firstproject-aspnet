1e30dc3ed9bcd62bcfeb5b37bafe27c54a4b8a0c
Roberto Prado <robertopfigue@gmail.com>
2020-04-08 00:46:54 -0300

    Adicionamos as propriendades Seller e SalesRecord

--- FirstProject/Data/FirstProjectContext.cs
+++ FirstProject/Data/FirstProjectContext.cs
@@ -13,6 +13,8 @@ namespace FirstProject.Models
         {
         }
 
-        public DbSet<FirstProject.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
--- FirstProject/Migrations/20200408034401_Initial.Designer.cs
+++ FirstProject/Migrations/20200408034401_Initial.Designer.cs
@@ -4,14 +4,16 @@ using FirstProject.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FirstProject.Migrations
 {
     [DbContext(typeof(FirstProjectContext))]
-    partial class FirstProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20200408034401_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
